f6a2b3d437b9e92c56ff07395964c9cf015a02cd
Nathaniel Lubranicki <ethqnit@gmail.com>
2025-12-04 00:59:23 -0500

    Initial commit: The Greatest C# program of all time

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main()
+        {
+            string value1 = "I";
+            string value2 = "Am";
+            string value3 = "The";
+            string value4 = "Funny";
+            string value5 = "Equal";
+            string value6 = "Tweeny";
+
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}",
+                value1, value2, value3, value4, value5, value6);
+                
+            // Output: I, Am, The, Funny, Equal, Tweeny
+        }
+    }
+}
